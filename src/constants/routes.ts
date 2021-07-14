1d1d270e0328d4ded443fdcc66c9cebca8734278
João Pedro Scarabelli Silva <jp_scarabelli@hotmail.com>
2021-07-14 15:32:49 -0300

    Add summary page, route and service

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -2,6 +2,7 @@ import PageNotFound from '../pages/404';
 import Login from '../pages/Login';
 import Brands from '../pages/Brands';
 import Autos from '../pages/Autos';
+import Summary from '../pages/Summary';
 import { RouteTypes } from '../types/routes';
 
 export const routes: RouteTypes[] = [
@@ -21,6 +22,12 @@ export const routes: RouteTypes[] = [
         path: "/veiculos",
         isPrivate: true,
         component: Autos,
+    },
+    {
+        exact: true,
+        path: "/dashboard",
+        isPrivate: true,
+        component: Summary,
     },
     {
         component: PageNotFound,
