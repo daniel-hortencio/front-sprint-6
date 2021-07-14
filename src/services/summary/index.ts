256937bac104cd9a009dba0dc3a7f907155f0ae6
Daniel Hortencio <hortencio.daniel@gmail.com>
2021-07-14 13:53:36 -0400

    Atualização de tipo de carro e listagem de carros exibindo marca

--- src/services/summary/index.ts
+++ src/services/summary/index.ts
@@ -2,7 +2,7 @@ import { AutoTypes } from '../../types/autos'
 import { api } from '../api'
 
 export const getAutos = async (): Promise<AutoTypes[]> => {
-    return api.get("/autos").then(({ data }) => {
+    return api.get("/summary").then(({ data }) => {
         console.log(data)
         const autos = data.map((auto: any) => {
             return {
